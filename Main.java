a0406bf1f00c119af94f9c26696ede18e1777da9
bwhitmore7 <bryson.whitmore@patelhs.org>
2020-11-05 15:28:33 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,5 @@
+class Main {
+  public static void main(String[] args) {
+    string[] cats = {"bengal cat", "Ragdoll","exotic shorthair", "Balinese cat", "munchkin cat", "chartreux cat", "havana brown", "toyger", "snowshoe cat", "colorpoint shorthair", "burmila cat", };
+  }
+}
