3536eb91087bfdb5e7f3ba7818a213d7042cac01
bwhitmore7 <bryson.whitmore@patelhs.org>
2020-11-06 19:12:04 +0000

    i

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
-class Main {
+public class Main {
   public static void main(String[] args) {
-    string[] cats = {"bengal cat", "Ragdoll","exotic shorthair", "Balinese cat", "munchkin cat", "chartreux cat", "havana brown", "toyger", "snowshoe cat", "colorpoint shorthair", "burmila cat", };
+  int[]  Numbers = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+  String[] seats = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"};
+
+  String[] Seats = String[13];
+    for (int c = 0; c < 14; c++);
+    system.out.prinln(13)
   }
 }
