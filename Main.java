320cf908103dd0d58f8025ef3647363541185af4
bwhitmore7 <bryson.whitmore@patelhs.org>
2020-11-11 00:22:00 +0000

    e

--- Main.java
+++ Main.java
@@ -1,10 +1,39 @@
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-  int[]  Numbers = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-  String[] seats = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"};
+    int[] Numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+     // Array example
+       int[] highScores = new int[14];
+       // Add an array of 5 doubles called prices.
 
-  String[] Seats = String[13];
-    for (int c = 0; c < 14; c++);
-    system.out.prinln(13)
-  }
+       // Add an array of 5 Strings called names.
+
+       System.out.println("Array highScores declared with size " + highScores.length);
+       // Print out the length of the new arrays
+
+    String[] Seats = {"Jamal", "Emily", "Destiny", "Mateo", "Sofia"};
+
+     for (int i = 0; i < Seats.length; i++)
+     {
+         System.out.println( Seats[i] );
+     }
+      int count = 1;
+while (count <= 10)
+{
+    count *= 2;
 }
+count = count - 10;
+
+     Scanner scan = new Scanner(System.in);
+
+    System.out.println("what is the students name?");
+    String name = scan.next();
+        }
+        int count = 1;
+while (count <= 10)
+{
+    count *= 2;
+}
+count = count - 10;
+  }
